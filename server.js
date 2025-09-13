bc5e9e41a12b598f1ae701a6e70a00951c7d88da
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-13 18:03:11 +0530

    solving cors error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL||'http://localhost:5173',
+    origin: process.env.FRONTEND_URL||'https://food-pharma.netlify.app',
     credentials: true
 }))
 
